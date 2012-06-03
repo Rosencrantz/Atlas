12e5bdfcca93563bdb6f2b63505345527a56d01b
Steve Haffenden <shaffenden@atlassian.com>
2012-06-04 09:28:40 +1000

    Updated the dropdown documentation.

--- js/dropdown.js
+++ js/dropdown.js
@@ -19,68 +19,69 @@
  * of the trigger. The container can be any element on the page and can contain any markup as long as 
  * the aria-owns values matches the id of the container.
  */
-define(['jquery', 'eventHandlers/visibilityHandler', 'mixins/panelMixin', 'mixins/relativePositionMixin', 'mixins/registerPluginMixin'], function ($, visibility, panel, positioning, registerPlugin) {
-    var trigger = '[data-trigger~="dropdown"]';
+define(['jquery', 
+    'eventHandlers/visibilityHandler', 
+    'mixins/panelMixin', 
+    'mixins/relativePositionMixin', 
+    'mixins/registerPluginMixin'], 
+    function ($, visibility, panel, positioning, registerPlugin) {
+        var trigger = '[data-trigger~="dropdown"]';
 
-    var Dropdown = function (element) {
-        $(element).on('click.dropdown', trigger, this.toggle);
-        pop = element;
-    };
+        var Dropdown = function (element) {
+            $(element).on('click.dropdown', trigger, this.toggle);
+            pop = element;
+        };
 
-     Dropdown.prototype = {
-        //Inverts whatever state the container is currently in. If displayed then hide, if hidden then display.
-        toggle : function (e) {
-            var that = $(this),
-                container = $('#' + that.attr('aria-owns')),
-                isHidden = visibility.isHidden(container);
+         Dropdown.prototype = {
+            toggle : function (e) {
+                var that = $(this),
+                    container = $('#' + that.attr('aria-owns')),
+                    isHidden = visibility.isHidden(container);
 
-            if(isHidden) {                
+                if(isHidden) {                
+                    open.call(this, e);
+                } else {
+                    close.call(this, e);
+                }
+
+                return !isHidden;
+            },
+            
+            open : function (e) {
                 open.call(this, e);
-            } else {
+            },
+
+            close : function (e) {
                 close.call(this, e);
             }
+        };
 
-            return !isHidden;
-        },
-        
-        open : function (e) {
-            open.call(this, e);
-        },
-
-        close : function (e) {
-            close.call(this, e);
+        function open(e) {
+            var element = $(this),
+                panel = element.data('panel');
+            
+            panel.open(function () {
+                var position = positioning(panel.container);
+                position.bottom().left().nudge({"left" : panel.container.outerWidth()});
+            });
         }
-    };
 
-    //Always opens the container, regardless of it's current state
-    function open(e) {
-        var element = $(this),
-            panel = element.data('panel');
-        
-        debugger;
-        panel.open(function () {
-            var position = positioning(panel.container);
-            position.bottom().left().nudge({"left" : panel.container.outerWidth()});
+        function close(e) {
+            $(trigger).each(function () {
+                var that = $(this),
+                    panel = that.data('panel');
+
+                panel.close();   
+            });
+            return false;
+        }
+
+        registerPlugin('dropdown', Dropdown);
+
+        $(function () {
+            $('html').on('click.dropdown.data-api', close);
+            $('body').on('click.dropdown.data-api', trigger, Dropdown.prototype.toggle);
         });
-    }
 
-    //Closes the container, regardless of it's current state
-    function close(e) {
-        $(trigger).each(function () {
-            var that = $(this),
-                panel = that.data('panel');
-
-            panel.close();   
-        });
-        return false;
-    }
-
-    registerPlugin('dropdown', Dropdown);
-
-    $(function () {
-        $('html').on('click.dropdown.data-api', close);
-        $('body').on('click.dropdown.data-api', trigger, Dropdown.prototype.toggle);
-    });
-
-    return Dropdown;
+        return Dropdown;
 });
