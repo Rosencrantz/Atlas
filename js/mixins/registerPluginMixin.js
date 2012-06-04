0894d8fffe8ecb3b051f08d435c436a74d013fa5
Steve Haffenden <shaffenden@atlassian.com>
2012-06-05 09:27:20 +1000

    Some worrying typo's in the registerPlugin src

--- js/mixins/registerPluginMixin.js
+++ js/mixins/registerPluginMixin.js
@@ -3,9 +3,9 @@ define(['jquery'], function ($) {
         $.fn[pluginName] = function (option) {
             return this.each(function () {
                 var $this = $(this),
-                    data = $this.data('tab');              
+                    data = $this.data(pluginName);              
                 
-                !data && $this.data('tab', (data = new plugin(this)));
+                !data && $this.data(pluginName, (data = new plugin(this)));
                 typeof option == 'string' && data[option].call($this);
             });
         }
