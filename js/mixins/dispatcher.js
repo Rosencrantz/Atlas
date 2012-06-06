8f5b55b23c73145b4eb734335d73ea529aa786f9
Steve Haffenden <stev.haffenden@gmail.com>
2012-06-06 22:16:22 +1000

    Removed panel documentation from the dispatcher, it's not valid any more

--- js/mixins/dispatcher.js
+++ js/mixins/dispatcher.js
@@ -1,33 +1,6 @@
 /* 
- * === Panel (internal use only) ===
- *
- * Panel is an internal convienence object for handling the visibility of trigger/container style plugins.
- * Panel iterates over the dom and finds all instances of 'panelAttribute' (aria-owns is the default) for
- * each of these it creates a new panel object which it attaches to the trigger element as a data-attribute.
- * 
- * Panel objects are used in plugins like the dropdown for displaying the panel on click/keydown etc. Calling 
- * panel.show or panel.hide generates events that can then be hooked by the plugin to add extra functionality 
- * such as positioning or validation.
- *
- * === Markup ===
- * 
- * <a aria-owns="panel">Trigger</a>
- * <div id="panel">Panel</div>
- *
- * === Javascript ===
- *
- * var panel = Panel(element);
- * panel.open();
- * panel.close();
- *
- * === Events ===
- * 
- * appName.show.pluginName.panel
- * appName.shown.pluginName.panel
- * appName.hide.pluginName.panel
- * appName.hidden.pluginName.panel
- * 
- */ 
+ * === Dispatcher (internal use only) ===
+ */
 define(['jquery', 'eve', 'settings', 'eventHandlers/controlLifecycle'], function ($, eve, settings, control) {
     return {
         dispatch : function _dispatch(eventName) {
