ea68d4a142aa39b3c1314bf75ab4cc425804d472
Steve Haffenden <stev.haffenden@gmail.com>
2012-06-06 22:12:41 +1000

    Added documentation for the tab plugin

--- js/tab.js
+++ js/tab.js
@@ -1,3 +1,36 @@
+/*
+ * === Tabs ===
+ *
+ * Tabs can be described as a collection of panels with a trigger associated with each. If a trigger is 
+ * activated then the associated panel is displayed (hiding all other panels). There is always one panel
+ * open.
+ *
+ * === Markup ===
+ * <div id="myTab" data-trigger="tab">
+ *     <a aria-owns="panel1">Panel 1 trigger</a>
+ *     <a aria-owns="panel2">Panel 2 trigger</a>
+ *     <a aria-owns="panel3">Panel 3 trigger</a>
+ *     <div id="panel1">Panel 1</div>
+ *     <div id="panel2">Panel 2</div>
+ *     <div id="panel3">Panel 3</div>
+ * </div>
+ *
+ * === Javascript ===
+ * 
+ * Note: In this example the selector is assumed to contain the appropriate 
+ * triggers and panels.
+ *
+ * var selector = $('#myTab');
+ * selector.tab();
+ *
+ * === Events ===
+ * 
+ * appName.show.tab -> raised before the panel is displayed to the user
+ * appName.shown.tab -> raised after the panel is displayed to the user
+ * appName.hide.tab -> raised before the panel is hidden from the user 
+ * appName.hidden.dtab -> raised after the panel is hidden from the user
+ *
+ */
 define(['jquery', 'settings', 'eventHandlers/controlLifecycle', 'mixins/dispatcher', 'mixins/relativePosition', 'mixins/register'], function ($, settings, control, dispatcher, position, register) {
     var trigger = '[data-' + settings.pluginAttribute + '="tab"]';
 
